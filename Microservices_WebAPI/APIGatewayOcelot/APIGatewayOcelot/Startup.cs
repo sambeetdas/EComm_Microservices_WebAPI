fbbe7d2de87c8b0c7e045498aff4806db932f897
Sambeet Das <sambeet.gugul@gmail.com>
2020-07-29 01:44:19 +0530

    Added Auth.JWT and updated Ocelot

--- Microservices_WebAPI/APIGatewayOcelot/APIGatewayOcelot/Startup.cs
+++ Microservices_WebAPI/APIGatewayOcelot/APIGatewayOcelot/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Ocelot.Middleware;
 using Ocelot.DependencyInjection;
+using Auth.JWT;
 
 namespace APIGatewayOcelot
 {
@@ -27,6 +28,9 @@ namespace APIGatewayOcelot
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Added for Auth.JWT
+            services.AddAuthService();
+
             services.AddOcelot(Configuration);
         }
 
